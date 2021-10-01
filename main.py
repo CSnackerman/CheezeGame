2959d775fa1daf92836fe955a85d2b00644861a4
Colby <colbyackerman@gmail.com>
2021-10-01 01:23:26 -0500

    formatting

--- main.py
+++ main.py
@@ -4,23 +4,18 @@ from window import Window
 from events import EventHandler
 from menu import Menu
 
-
+# --- Setup ---
 pygame.init()
-
 window = Window ('Wise Cheeze')
-
 
 # scenes
 menu = Menu()
 
 
-# main loop
+# --- Main Loop ---
 while 1:
 
     EventHandler.pollEvents()
-
     window.clear()
-
     menu.display (window.surface)
-
     window.update()
